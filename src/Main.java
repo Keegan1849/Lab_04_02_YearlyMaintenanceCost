69f4d11b0df29f22f1f6b63fa75e3b6b22e8879c
[Keegan Chapin] <[keeg.chapin@gmail.com]>
2023-09-14 13:46:06 -0400

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double summer = 243.53;
+        double winter = 274.43;
+        double fall = 132.23;
+        double spring = 340.50;
+        double total = 0.0;
+        total = summer + winter + fall + spring;
+
+        System.out.println("The summer cost is " + summer + " and the winter cost is " + winter + " and the fall cost " + fall + " And the spring cost " + spring);
+        System.out.println("The total cost is " + total);
     }
 }
